fb801691286903269c1d5ebcaa26d8cbec0f27d3
gehal6 <g.halim@waslabrowser.com>
2023-11-26 12:24:23 +0200

    modified:   backend.tf

--- backend.tf
+++ backend.tf
@@ -5,7 +5,5 @@ terraform {
     region         	   = "eu-central-1"
     encrypt        	   = true
     dynamodb_table         = "terraform-state-lock-dynamo"
-    access_key             = "AKIAVJAHCUXAFV4EAQFQ"
-    secret_key             = "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"
   }
 }
